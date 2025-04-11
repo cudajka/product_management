da85342c324549f80d271576cda1226426e0c117
cudajka <phucdatcu@gmail.com>
2025-04-11 21:31:39 +0700

    Create migration

--- database/migrations/2025_04_11_130945_create_products_table.php
+++ database/migrations/2025_04_11_130945_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->bigInteger('price');
+            $table->bigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('product_categories')->onDelete('cascade');
+            $table->foreignId('brand_id')->references('id')->on('brands')->onDelete('cascade');
+            $table->longText('description');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
