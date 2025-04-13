372487989e4ea7d4ace4d605dcb7d817d044d9cd
cudajka <phucdatcu@gmail.com>
2025-04-13 10:27:29 +0700

    Add missing file

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Color extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'colors';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = true;
+    protected $fillable = [];
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasAttributes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory, HasAttributes;
+    protected $table = 'products';
+    protected $primaryKey = 'id';
+    protected $fillable = [''];
+    public $incrementing = true;
+    public $timestamps = true;
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+    const DELETED_AT = 'deleted_at';
+    public function product_categories(){
+        return $this->belongsTo(ProductCategory::class, 'product_id')->withDefault();
+    }
+    public function brands(){
+        return $this->belongsTo(Brand::class, 'brand_id')->withDefault();
+    }
+    public function colors(){
+        return $this->belongsToMany(Color::class, 'product_variants');
+    }
 }
